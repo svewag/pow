4d1339ec7c6269351937317d8623f847792d0f6f
Sven Wagner <svenw@posteo.de>
2020-06-14 14:40:26 +0200

    initial commit

---
+++ pages/battery.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <battery />
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import battery from '~/components/battery'
+
+export default {
+  components: {
+    battery
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <nuxt-link to="battery"><v-btn large>Battery</v-btn></nuxt-link>
+    </v-flex>
+  </v-layout>
+</template>
